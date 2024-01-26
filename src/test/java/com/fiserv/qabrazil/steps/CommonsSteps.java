66a21dd9b71e4f0b0c3bffbff3e8678d2ee04059
fcc9vuv <luckMe23@#@#>
2024-01-26 15:51:39 -0300

    getting screenshot when fail

--- src/test/java/com/fiserv/qabrazil/steps/CommonsSteps.java
+++ src/test/java/com/fiserv/qabrazil/steps/CommonsSteps.java
@@ -1,6 +1,9 @@
 package com.fiserv.qabrazil.steps;
 
 import com.fiserv.qabrazil.pages.CommonsPage;
+import com.microsoft.playwright.Page;
+import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
 import io.cucumber.java.en.Then;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -10,6 +13,18 @@ public class CommonsSteps {
 
     @Autowired
     CommonsPage commonsPage;
+
+    @Autowired
+    Page page;
+
+    @After
+    public void tearDown(Scenario scenario) {
+        if (scenario.isFailed()) {
+            scenario.attach(page.screenshot(new Page.ScreenshotOptions().setFullPage(true)),
+                    "image/png", "Screen Shot");
+            scenario.attach(page.content(), "text/html", "Content");
+        }
+    }
 
     @Then("Usuário verá mensagem {string}")
     public void userWillSeeMessage(String expectedMessage) {
