f8c8e4987aae495f6aaf6350b47c1bf0c940dc39
Herbert Weigert <herbert.venancio@fiserv.com>
2024-02-05 10:08:04 -0300

    implemented retry of failed scenarios

--- src/test/java/com/fiserv/qabrazil/CucumberTest.java
+++ src/test/java/com/fiserv/qabrazil/CucumberTest.java
@@ -1,10 +1,24 @@
 package com.fiserv.qabrazil;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.FeatureWrapper;
+import io.cucumber.testng.PickleWrapper;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 public class CucumberTest extends AbstractTestNGCucumberTests {
-    @DataProvider()
+
+    @Override
+    @Test(
+            groups = "cucumber",
+            description = "Runs Cucumber Scenarios",
+            dataProvider = "scenarios",
+            retryAnalyzer = RetryOnFailure.class)
+    public void runScenario(PickleWrapper pickleWrapper, FeatureWrapper featureWrapper) {
+        super.runScenario(pickleWrapper, featureWrapper);
+    }
+
+    @DataProvider
     @Override
     public Object[][] scenarios() {
         return super.scenarios();
