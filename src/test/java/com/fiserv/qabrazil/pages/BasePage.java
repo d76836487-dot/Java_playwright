2e288dca00fa212aa16bcfdbfb843161008af932
fcc9vuv <luckMe23@#@#>
2024-02-08 14:19:30 -0300

    scenario for future receivables matching api - sales and prepayment api

--- src/test/java/com/fiserv/qabrazil/pages/BasePage.java
+++ src/test/java/com/fiserv/qabrazil/pages/BasePage.java
@@ -70,7 +70,7 @@ public abstract class BasePage {
     }
 
     public Number getNumberFromCurrencyElement(String testId) {
-        waitUntilTrue(90, () -> !getTextFromElement(testId).equals("R$ 0,00")); // it returns R$ 0,00 before setting the real value...
+        waitUntilTrue(() -> !getTextFromElement(testId).equals("R$ 0,00")); // it returns R$ 0,00 before setting the real value...
         String textFromElement = getTextFromElement(testId);
         try {
             return Currency.parseCurrency(textFromElement);
