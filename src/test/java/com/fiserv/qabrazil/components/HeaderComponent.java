ad62da0f04295a417b2fc9bb21de3e450e7dbac0
Daniel Wandarti <daniel.wandarti@fiserv.com>
2024-03-04 15:43:40 -0300

    small fix

--- src/test/java/com/fiserv/qabrazil/components/HeaderComponent.java
+++ src/test/java/com/fiserv/qabrazil/components/HeaderComponent.java
@@ -2,19 +2,17 @@ package com.fiserv.qabrazil.components;
 
 import com.fiserv.automation.framework.annotations.ScenarioComponent;
 import com.fiserv.qabrazil.pages.BasePage;
-import com.fiserv.qabrazil.util.Identifier;
-import com.microsoft.playwright.Locator;
-import com.microsoft.playwright.Page;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.fiserv.qabrazil.pages.PageField;
+
+import static com.fiserv.qabrazil.util.WaitUtil.waitUntilTrue;
 
 @ScenarioComponent
 public class HeaderComponent extends BasePage {
-    @Autowired
-    Page page;
-
     public void selectShowValuesButton(boolean show) {
-        Locator button = page.getByTestId(Identifier.from("Header - Ocultar Valores").testId());
-        if (button.getAttribute("status").equals("on") == show) return;
+        PageField button = pageField.from("Header - Ocultar Valores");
+        waitUntilTrue(button::fieldIsOneVisibleAndEnabled);
+
+        if (button.getLocator().getAttribute("status").equals("on") == show) return;
 
         button.click();
     }
