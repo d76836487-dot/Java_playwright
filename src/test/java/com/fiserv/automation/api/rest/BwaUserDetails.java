ebdc07ac0caeb4be11d10d7c8eada27a3218aa95
Daniel Wandarti <daniel.wandarti@fiserv.com>
2024-03-12 14:06:33 -0300

    added log into BwaUserDetails

--- src/test/java/com/fiserv/automation/api/rest/BwaUserDetails.java
+++ src/test/java/com/fiserv/automation/api/rest/BwaUserDetails.java
@@ -1,20 +1,31 @@
 package com.fiserv.automation.api.rest;
 
 import com.fiserv.automation.api.dto.UserDetailDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import retrofit2.Call;
 import retrofit2.Response;
 
 @Component
 public class BwaUserDetails extends BwaBase {
+    private static final Logger log = LoggerFactory.getLogger(BwaUserDetails.class);
 
     public UserDetailDto getUserDetails(String apiAccessToken) throws Exception {
         BwaRest bwaRest = bwaHeader.getBwaRequest(apiAccessToken);
-        Response<UserDetailDto> execute = bwaRest.userDetail().execute();
+        Call<UserDetailDto> userDetailDtoCall = bwaRest.userDetail();
+        Response<UserDetailDto> execute = userDetailDtoCall.execute();
         if (execute.code() != 200) {
             String errorBody = "";
             if (execute.errorBody() != null) {
                 errorBody = execute.errorBody().string();
             }
+
+            log.info("request url: %s".formatted(userDetailDtoCall.request().url()));
+            log.info("request headers: %s".formatted(userDetailDtoCall.request().headers()));
+            log.info("access_token: %s".formatted(apiAccessToken));
+            log.info("headers: %s".formatted(execute.headers()));
+            log.info("errorBody: %s".formatted(errorBody));
 
             throw new Exception(
                     String.format("Erro ao obter detalhes do usuário %s: %s. %s", execute.code(), execute.message(), errorBody));
