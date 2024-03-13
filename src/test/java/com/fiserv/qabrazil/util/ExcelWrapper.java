1f37554cb8d33581fbab75ce9a72597e5bfbb8fd
Daniel Wandarti <daniel.wandarti@fiserv.com>
2024-03-13 15:23:28 -0300

    ExcelWrapper

---
+++ src/test/java/com/fiserv/qabrazil/util/ExcelWrapper.java
@@ -0,0 +1,52 @@
+package com.fiserv.qabrazil.util;
+
+import org.dhatim.fastexcel.reader.ReadableWorkbook;
+import org.dhatim.fastexcel.reader.Sheet;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExcelWrapper implements AutoCloseable {
+    private final FileInputStream fis;
+    private final ReadableWorkbook workbook;
+    private final Sheet sheet;
+    private final int rowTableStart;
+
+    public ExcelWrapper(String filename, int rowTableStart) throws IOException {
+        fis = new FileInputStream(filename);
+        workbook = new ReadableWorkbook(fis);
+        sheet = workbook.getFirstSheet();
+        this.rowTableStart = rowTableStart;
+    }
+
+    @Override
+    public void close() throws Exception {
+        workbook.close();
+        fis.close();
+    }
+
+    public List<String> getColumnsAsText(String columnName) throws IOException {
+        List<String> values = new ArrayList<>();
+        int col = getHeaderColumn(columnName);
+
+        for(int row = rowTableStart + 1; row < sheet.read().size(); row++) {
+            values.add(getCellAsText(row, col));
+        }
+
+        return values;
+    }
+
+    private int getHeaderColumn(String columnName) throws IOException {
+        for(int col = 0; col < sheet.read().get(0).getCellCount(); col++) {
+            if (getCellAsText(rowTableStart, col).equals(columnName)) return col;
+        }
+
+        throw new RuntimeException("Coluna %s do excel não foi encontrada".formatted(columnName));
+    }
+
+    public String getCellAsText(int row, int column) throws IOException {
+        return sheet.read().get(row).getCell(column).getText();
+    }
+}
