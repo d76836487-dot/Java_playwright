04b343234015b280bbe6223a7ff503e31c2d3253
Daniel Wandarti <daniel.wandarti@fiserv.com>
2024-04-17 09:12:32 -0300

    smallfix on reportTypeIsCorrect

--- src/test/java/com/fiserv/qabrazil/steps/ReportsSteps.java
+++ src/test/java/com/fiserv/qabrazil/steps/ReportsSteps.java
@@ -57,7 +57,7 @@ public class ReportsSteps extends BaseSteps {
 
     private static void reportTypeIsCorrect(String fileType) {
         final String message = String.format("Tipo do relatório \"%s\" é diferente de \"Vendas\" e \"Pagamentos\"", fileType);
-        final List<String> allowedFileTypes = List.of("Vendas", "Recebimentos");
+        final List<String> allowedFileTypes = List.of("Vendas", "Pagamentos");
         assertTrue(message, allowedFileTypes.contains(fileType));
     }
 
