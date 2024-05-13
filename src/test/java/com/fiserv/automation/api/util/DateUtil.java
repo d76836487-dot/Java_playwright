004bccfdf9ece13c491b96016071d4690d3d9c45
Daniel Wandarti <daniel.wandarti@fiserv.com>
2024-05-13 14:05:20 -0300

    scenario Consultar Período (Essa semana)

--- src/test/java/com/fiserv/automation/api/util/DateUtil.java
+++ src/test/java/com/fiserv/automation/api/util/DateUtil.java
@@ -12,6 +12,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.TextStyle;
+import java.time.temporal.TemporalAdjusters;
 import java.util.Calendar;
 import java.util.Locale;
 
@@ -43,18 +44,10 @@ public class DateUtil {
                     .minusDays(1);
             case "dia de sete dias atrás", "data de sete dias atrás" -> LocalDate.now().minusDays(7);
             case "dia de ontem", "data de ontem" -> LocalDate.now().minusDays(1);
-            case "início da semana" -> lookForDayWeek(DayOfWeek.SUNDAY, -1);
-            case "fim da semana" -> lookForDayWeek(DayOfWeek.SATURDAY, 1);
+            case "início da semana" -> LocalDate.now().with(TemporalAdjusters.previousOrSame(DayOfWeek.SUNDAY));
+            case "fim da semana" -> LocalDate.now().with(TemporalAdjusters.nextOrSame(DayOfWeek.SATURDAY));
             default -> throw new IllegalStateException("Unexpected value: " + expectedDate);
         };
-    }
-
-    private static LocalDate lookForDayWeek(DayOfWeek dayOfWeek, int advanceDay) {
-        LocalDate day = LocalDate.now();
-        while (day.getDayOfWeek().compareTo(dayOfWeek) != 0) {
-            day = day.plusDays(advanceDay);
-        }
-        return day;
     }
 
     @PostConstruct
